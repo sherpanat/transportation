47aa65128b6227f997be244be8e273c3cf41b52e
Nathan Huberty <nathan.huberty@orange.fr>
2019-09-05 01:32:20 +0200

    Add change of status of a Journey in Rails.logger.info

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,12 +3,21 @@ class Journey < ApplicationRecord
   enum status: { created: 'created', started: 'started', cancelled: 'cancelled' }
 
   before_create :set_unique_code
+  after_save :log_change_of_status, if: :status_changed?
 
   def set_unique_code
     self.code = self.class.unique_code
   end
 
   private
+
+  def log_change_of_status
+    Rails.logger.info "Status of journey number #{id} (code: #{code}) changed from #{previous_changes[:status][0]} to #{previous_changes[:status][1]}"
+  end
+
+  def status_changed?
+    previous_changes[:status]
+  end
 
   def self.unique_code
     code = ""
